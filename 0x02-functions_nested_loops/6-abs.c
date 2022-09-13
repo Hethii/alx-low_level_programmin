8a4969215f35b3d72cc6ca9e5b51f6e4bc4288b7
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-13 16:04:26 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * @n: integer to make absolute
+ *
+ * Return: int or int times negative
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
