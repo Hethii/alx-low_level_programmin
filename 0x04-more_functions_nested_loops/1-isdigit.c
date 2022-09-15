057cc6c56ef65aeaaf864fcbe144a167d7a90122
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-15 16:54:05 -0700

    _isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,19 +3,15 @@
 /**
  * _isdigit -  a function that checks for a digit 0-9
  *
- * @c: input for number
+ * @c: input
  *
- * Return: 1 if its uppercase, 0 if not
+ * Return: 1 if its digit, 0 if not
  */
 
 int _isdigit(int c)
 {
-	if (c >= '48' && c <= '57')
-	{
+	if (c >= 48 && c <= 57)
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
