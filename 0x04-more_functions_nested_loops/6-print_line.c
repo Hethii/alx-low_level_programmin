811f0d3cf86d57128f94785476c342f8939b6c69
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-16 14:17:11 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_line - print a straight line
  *
- * @n: is the number of times the _ character
- * should be printed
+ * @n: is the number of times the _
+ * character should be printed
  */
 
 void print_line(int n)
