9346614377ec0b5a3f3aca0f287a761a6e5a6b14
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-16 12:52:47 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character
+ * should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
