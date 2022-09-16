dfb29735981da7316f118243f66fa6db16666b71
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-16 14:42:47 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ *
+ * @n: is the number of times the # character
+ * Return: a square  line
+ */
+
+void print_sqaure(int size)
+{
+	int co, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			for (hash = 1; hash <= size; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
