b6d7d2ec8094dc1eec6fc13c6b4e6d4944b5559c
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-16 20:54:08 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,7 +19,7 @@ void print_number(int n)
 
 	/*print the first few digits*/
 	if ((num / 10) > 0)
-		_putchar(num / 10);
+		print_number(num / 10);
 
 	/*print the last digit*/
 	_putchar((num % 10) + 48);
