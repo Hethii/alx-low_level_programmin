508a1b3ae991cb203fb57bcbd37fd20561f31bc9
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-26 15:30:07 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,6 @@
  * @size: width of matrix column
  *
  * Return: void
- *
  */
 
 void print_diagsums(int *a, int size)
@@ -25,5 +24,6 @@ void print_diagsums(int *a, int size)
 		p = (j * size) + (size - 1 - j);
 		r += *(a + p);
 	}
+
 	printf("%i, %i\n", l, r);
 }
