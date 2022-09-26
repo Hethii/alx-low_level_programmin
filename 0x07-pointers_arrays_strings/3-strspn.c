c224fdd7ab42b6a02f5225d78c94efc505e57207
Ebenezer Owoeye <ezertobi@gmail.com>
2022-09-26 15:01:12 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strspn - gets lengthof a prefix substring\
+ * _strspn - gets lengthof a prefix substring
  * @s: string to check
  * @accept: string to check against
  *
@@ -24,6 +24,4 @@ unsigned int _strspn(char *s, char *accept)
 			break;
 	}
 	return (i);
-
-
 }
