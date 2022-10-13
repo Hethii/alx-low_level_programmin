d98cad4117684d03ff24159a7d3659903f06b752
Hethii <ezertobi@gmail.com>
2022-10-13 16:40:46 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,33 +1,32 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_numbers - a function that print numbers, followed by a new line.
- *
- * @separator: pointer to constant separator
- * @n: start of input variables
- *
- * Return: nothing
+ * print_strings - prints a string
+ * @separator: seperator char
+ * @n: number of args
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int index;
+	unsigned int i;
+	va_list stringArgs;
+	char *strArg;
 
-	/* initialize the argument list from the start */
-	va_start(ap, n);
-
-	/* iterate through each argument*/
-	for (index = 0; index < n; index++)
+	va_start(stringArgs, n);
+	for (i = 0; i < n; i++)
 	{
-		/* print next argument */
-		printf("%d", va_arg(ap, int));
-		/* print separator only between arguments */
-		if (separator && index != n - 1)
+		strArg = va_arg(stringArgs, char *);
+
+		if (separator != NULL && i > 0)
 			printf("%s", separator);
+		if (strArg == NULL)
+			printf("(nil)");
+		else
+			printf("%s", strArg);
 	}
-	/*clean up*/
-	va_end(ap);
+	va_end(stringArgs);
 	printf("\n");
 }
